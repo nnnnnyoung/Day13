ebd16bacd146fb2474498d2bf6ae3d59720c4c04
nnnnnyoung <112464074+nnnnnyoung@users.noreply.github.com>
2022-09-16 16:29:56 +0900

    Add files via upload
    자동차 객체 만들기

--- src/kkk/Main.java
+++ src/kkk/Main.java
@@ -1,17 +1,9 @@
+package Board;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Goods g=new Goods();
-		
-		g.info();
-		g.n();
-		g.inputPrice(1000);
-		g.info();
-		
-		g.input("aa", 100, 1000);
-		g.info();
 
 	}
 
